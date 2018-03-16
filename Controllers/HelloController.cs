b3b3d75cead4c126873a367989086977de26a002
srikanthpragada <srikanthpragada@gmail.com>
2018-03-16 09:33:51 +0530

    16-MAR

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class HelloController : Controller
+    {
+       
+        // Action  Index 
+        public ActionResult Index()
+        {
+            return Content("Hello World. I am learning ASP.NET MVC");  // ContentResult
+        }
+
+        public ActionResult Wish(string name)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.Footer = "Srikanth Technologies";
+            return View();  // ViewResult 
+        }
+
+        public ActionResult About()
+        {
+            return Content("ASP.NET MVC Course");
+        }
+    }
+}
