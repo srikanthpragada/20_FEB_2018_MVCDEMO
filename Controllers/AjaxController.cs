e84be3707607ba749cf5c6c165c7955e7377f2ab
srikanthpragada <srikanthpragada@gmail.com>
2018-03-21 09:32:51 +0530

    21-MAR

---
+++ Controllers/AjaxController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class AjaxController : Controller
+    {
+        // GET: Ajax
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Now()
+        {
+            Thread.Sleep(10000);
+            return Content(DateTime.Now.ToString());
+        }
+
+        public ActionResult Add(int n1, int n2)
+        {
+            return Content( String.Format("Sum = {0}",n1 + n2) );
+        }
+    }
+}
