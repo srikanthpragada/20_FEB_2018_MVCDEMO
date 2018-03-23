a33830acd4aeed62157e82a08de57935e6c76f6b
srikanthpragada <srikanthpragada@gmail.com>
2018-03-23 09:32:39 +0530

    23-MAR

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace mvcdemo.Controllers
 {
     public class UserController : Controller
     {
-        
+
+        [Authorize]
+        public ActionResult Home()
+        {
+            return View();
+        }
+
         public ActionResult Login()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string pwd)
+        {
+            if (pwd == "admin")
+            {
+                FormsAuthentication.SetAuthCookie("admin", false);
+                return RedirectToAction("Home");
+            }
+            else
+                ViewBag.Message = "Sorry! Invaid Login. Try Again!";
+
             return View();
         }
     }
